5428d211b4f7b7245715e2fec4e1764487b4f3f8
TenpennyUMFPOS <med19aziz@gmail.com>
2025-07-08 13:32:15 +0100

    tp3 adding a couple of new models to blog

--- day 1/tp/mysite/blog/admin.py
+++ day 1/tp/mysite/blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Author, Category, Tag, Post, Comment
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_at')
+    list_filter = ('created_at', 'author', 'category')
+    search_fields = ('title', 'content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'email', 'created_at')
+    list_filter = ('created_at', 'post')
+    search_fields = ('name', 'email', 'content')
+
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Tag)
--- day 1/tp/mysite/blog/models.py
+++ day 1/tp/mysite/blog/models.py
@@ -1,9 +1,46 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
