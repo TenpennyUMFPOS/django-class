0c2ffae1853890c541b6f555df3de2500ba431e6
TenpennyUMFPOS <med19aziz@gmail.com>
2025-07-08 13:59:04 +0100

    grandtp partie 4 dashboard

--- day 2/grandprojet/jobs/views.py
+++ day 2/grandprojet/jobs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Avg, Count
+from .models import JobRecord
 
-# Create your views here.
+def dashboard(request):
+    total_jobs = JobRecord.objects.count()
+    avg_salary = JobRecord.objects.aggregate(Avg('salary_in_usd'))['salary_in_usd__avg'] or 0
+    num_countries = JobRecord.objects.values('employee_residence').distinct().count()
+
+    context = {
+        'total_jobs': total_jobs,
+        'avg_salary': round(avg_salary, 2),
+        'num_countries': num_countries,
+    }
+    return render(request, 'jobs/dashboard.html', context)
